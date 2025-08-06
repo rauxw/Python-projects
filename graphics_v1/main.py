cb950b39fc064a7af4871a3377268a620403e40f
rauxw <hardikraut909@gmail.com>
2025-08-06 14:00:32 +0530

    coffe

--- graphics_v1/main.py
+++ graphics_v1/main.py
@@ -1,6 +1,26 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-timmy = Turtle()
+# timmy = Turtle()
+# print(timmy)
 
-my_screen = Screen()
+# timmy.shape("turtle")
+# timmy.fillcolor("red")
+# timmy.forward(100)
 
+# my_screen = Screen()
+
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+table = PrettyTable()
+
+table.field_names = ["Pokemon Name", "Type"]
+
+table.add_row(["Pikachu","Electric"])
+table.add_row(["Squritle", "Water"])
+table.add_row(["Charmander", "Fire"])
+
+table.align = 'l'
+
+print(table)
