751cbbaaaee4d6889e8b90a3a93dbae2a5484e9b
rauxw <hardikraut909@gmail.com>
2025-08-15 22:20:09 +0530

    food collision

--- snake game/main.py
+++ snake game/main.py
@@ -24,7 +24,11 @@ game_is_on = True
 while game_is_on:
   screen.update()
   time.sleep(0.1)
+  snake.move()
 
-  snake.move( )
+  #Detect collision with food
+  if snake.head.distance(food) < 15:
+    food.refresh()
+
 
 screen.exitonclick()
