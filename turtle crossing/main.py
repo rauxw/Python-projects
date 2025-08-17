89d7f0ed6a21f8aa171969f858755451182650e8
rauxw <hardikraut909@gmail.com>
2025-08-17 19:02:19 +0530

    done

--- turtle crossing/main.py
+++ turtle crossing/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -10,8 +11,8 @@ screen.bgcolor("white")
 screen.tracer(0)
 
 player = Player()
-
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 
@@ -29,13 +30,14 @@ while game_is_on:
   for car in car_manager.all_cars:
     if car.distance(player) < 20:
       game_is_on = False
-      print("Collide")
+      scoreboard.game_over()
 
   #Detect successfully finish
   if player.is_at_finish_line():
     player.go_to_start()
     car_manager.level_up()
+    scoreboard.increase_level()
 
-  print(car_manager.car_speed)
+
 
 screen.exitonclick()
